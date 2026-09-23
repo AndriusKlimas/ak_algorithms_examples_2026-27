45d636a81a53c990364f315646c1bc2a944e73f9
AndriusKlimas <andrius.klimas01@gmail.com>
2026-09-23 11:26:42 +0100

    average done, and added java string

--- src/main/java/in_class/week2/ArrayUtils.java
+++ src/main/java/in_class/week2/ArrayUtils.java
@@ -1,9 +1,47 @@
 package in_class.week2;
 
 public class ArrayUtils {
-    public static void displayArray(int[] nums) {
-        for (int i = 0; i < nums.length; i++) {
-            System.out.println("Current number is " + nums[i] + ". Position is " + i);
+
+    /*
+    Takes in an array of Int and returns the order they are stored in
+
+    Parses int[] array - holds the array of int to be printed out
+     */
+    public static void displayArrayNums(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.println("Current number is " + array[i] + ". Position is " + i);
         }
+    }
+
+    /*
+    Takes in an array of strings and returns the order they are stored in
+
+    Parses String[] array - holds the array of strings to be printed out
+     */
+    public static void displayArrayString(String[] array){
+        for(int i=0;i<array.length;i++){
+            System.out.println("Current wordis "+array[i]+". Position is "+i);
+        }
+    }
+
+    /*
+    Takes in array of Int, prints out the average of all the number
+
+    Parses int[] array - an array of all in that needs to be averaged
+     */
+
+    public static void averageArrayInt(int[] array){
+        int current = 0;
+        int count = 0;
+        System.out.println("Test1");
+        for (int i=0;i<array.length;i++){
+            current = array[i] + current;
+            count++;
+            System.out.println(count);
+        }
+        if (count>0){
+            int average = current/count;
+            System.out.println("Average is "+ average);
+            }
     }
 }
--- src/main/java/in_class/week2/arraySpoof.java
+++ src/main/java/in_class/week2/arraySpoof.java
@@ -1,10 +1,13 @@
 package in_class.week2;
 
 public class arraySpoof {
-    static void main(){
-        int [] arrays = {32,31,321,212,3321,3214};
+    static void main() {
+        int[] arraysInt = {32, 31, 321, 212, 3321, 3214};
+        String [] arrayString = {"Dippsy","Po","Laalaa","TinkyWinky"};
+        int[] arrayAvr = {10,10,10,10,10};
 
-        ArrayUtils.displayArray(arrays);
-
+//        ArrayUtils.displayArrayNums(arraysInt);
+//        ArrayUtils.displayArrayString(arrayString);
+        ArrayUtils.averageArrayInt(arrayAvr);
     }
 }
